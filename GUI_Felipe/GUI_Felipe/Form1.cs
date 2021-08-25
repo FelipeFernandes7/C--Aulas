cff62ce312d6b7af76b09204a6427ac89f841fcb
Luiz Felipe <71227873+FelipeFernandes7@users.noreply.github.com>
2021-08-24 21:29:14 -0300

    Add files via upload

---
+++ GUI_Felipe/GUI_Felipe/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Felipe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Bom Dia " +  textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bom dia " + textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+              MessageBox.Show("Encerrando...");
+              Close();
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Deseja Encerrar?","Atenção!!",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, 
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else e.Cancel = true;
+        }
+    }
+}
